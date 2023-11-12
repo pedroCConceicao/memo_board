a47ae86a18c54b611ccf11d04ba9cd18b3bc1b9b
Pedro <pedrocconce@gmail.com>
2023-11-12 11:14:06 -0300

    Criando a base

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 
 import Post from "./Post/Post";
@@ -10,12 +11,16 @@ const Posts = () => {
 
 
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
-    )
+        !posts.lenght ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    );
 }
 
 export default Posts;
