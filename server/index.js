739c1bcfd805125ae267cac5957461948fdc0471
Pedro Carné <68451062+pedroCConceicao@users.noreply.github.com>
2023-11-27 22:07:21 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL  = 'mongodb+srv://admin:123Pedro@cluster0.60y5wjq.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL  = 'your_database_connection'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
